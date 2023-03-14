70a2fe7af3ede96435e2dc003ab0076c07ff08e7
Viraj76 <virajg.7680@gmail.com>
2023-03-14 20:02:41 +0530

    designed  the post view

--- app/src/main/java/com/example/bottomnavigation/HomeFragment.kt
+++ app/src/main/java/com/example/bottomnavigation/HomeFragment.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 
 
@@ -17,9 +15,8 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        return view
+        return inflater.inflate(R.layout.fragment_home, container, false)
 
     }
 //    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
