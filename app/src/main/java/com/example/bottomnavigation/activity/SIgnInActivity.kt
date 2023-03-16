d8d5331bd776a62d693b15a0f6b7b56d68ca225e
Viraj76 <virajg.7680@gmail.com>
2023-03-16 20:33:29 +0530

    Designing authentication pages

---
+++ app/src/main/java/com/example/bottomnavigation/activity/SIgnInActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bottomnavigation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SIgnInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
