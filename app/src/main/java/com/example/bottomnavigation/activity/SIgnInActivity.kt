a8a96e3adf3053240d62d5733511440c9a2c3f97
Viraj76 <virajg.7680@gmail.com>
2023-03-16 20:36:55 +0530

    Designed Sign In Layout

--- app/src/main/java/com/example/bottomnavigation/activity/SIgnInActivity.kt
+++ app/src/main/java/com/example/bottomnavigation/activity/SIgnInActivity.kt
@@ -1,11 +1,32 @@
 package com.example.bottomnavigation.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bottomnavigation.databinding.ActivitySignInBinding
 
 class SIgnInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding= ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnLogin.setOnClickListener { loggingUser() }
+        binding.tvSignUp.setOnClickListener { goingToSignUpActivity() }
+
+
+    }
+
+    private fun goingToSignUpActivity() {
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun loggingUser() {
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/bottomnavigation/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/bottomnavigation/fragments/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.bottomnavigation.fragments
 
+
 import android.os.Bundle
 
 import android.view.LayoutInflater
@@ -17,13 +18,11 @@ import com.example.bottomnavigation.databinding.FragmentHomeBinding
 import com.example.bottomnavigation.databinding.PostCardBinding
 import com.google.firebase.database.*
 
-
 class HomeFragment : Fragment() {
-
     private lateinit var binding: FragmentHomeBinding
     private lateinit var databaseReference: DatabaseReference
     private lateinit var postAdapter: PostAdapter
-    private lateinit var allClientsDataList : ArrayList<ClientDetails>
+    private lateinit var allClientsDataList: ArrayList<ClientDetails>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,42 +32,34 @@ class HomeFragment : Fragment() {
         preparePostRecyclerView()
         showingPostInTheRecyclerView()
         return binding.root
-
     }
 
     private fun showingPostInTheRecyclerView() {
         databaseReference = FirebaseDatabase.getInstance().getReference("Client Detail")
 
-        databaseReference.addValueEventListener(object :ValueEventListener{
+        databaseReference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                if(snapshot.exists()){
-                    for(allClientsData in snapshot.children){
+                if (snapshot.exists()) {
+                    for (allClientsData in snapshot.children) {
                         val clientsData = allClientsData.getValue(ClientDetails::class.java)
                         allClientsDataList.add(clientsData!!)
                     }
                     postAdapter.setPostList(allClientsDataList)
                 }
             }
+
             override fun onCancelled(error: DatabaseError) {
-                Toast.makeText(requireContext(),error.message,Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), error.message, Toast.LENGTH_SHORT).show()
             }
-
         })
-
-
     }
+
 
     private fun preparePostRecyclerView() {
         postAdapter = PostAdapter()
         binding.postRecyclerView.apply {
-            layoutManager = LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false)
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
             adapter = postAdapter
         }
     }
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//        val toolbar = view.findViewById<Toolbar>(R.id.toolBar)
-//        (activity as AppCompatActivity).setSupportActionBar(toolbar)
-//    }
 }
