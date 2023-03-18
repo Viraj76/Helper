bc022502f733d7c82ee4aaadd85e8463610e633a
Viraj76 <virajg.7680@gmail.com>
2023-03-18 23:51:29 +0530

    Implemented Sign Up functionality

---
+++ app/src/main/java/com/example/bottomnavigation/ClientID.kt
@@ -0,0 +1,8 @@
+package com.example.bottomnavigation
+
+data class ClientID(
+    val name :String? ="",
+    val email : String?="",
+    val password : String?="",
+    val clientId : String?=""
+)
---
+++ app/src/main/java/com/example/bottomnavigation/ContractorID.kt
@@ -0,0 +1,8 @@
+package com.example.bottomnavigation
+
+data class ContractorID(
+    val name :String?="",
+    val email : String?="",
+    val password : String?="",
+    val contractorId : String?=""
+)
