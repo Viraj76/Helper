8507a3faa8a8611d5728cbdaec6fdc99627a4853
Viraj76 <virajg.7680@gmail.com>
2023-06-01 07:57:26 +0530

    Quotation activity and layouts

--- app/src/main/java/com/example/bottomnavigation/MyData.kt
+++
@@ -1,5 +0,0 @@
-package com.example.bottomnavigation
-
-data class MyData(
-    val downloadUrls: List<String>
-)
---
+++ app/src/main/java/com/example/bottomnavigation/contractor/activity/QuestionsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bottomnavigation.contractor.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questions)
+    }
+}
