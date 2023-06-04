21c643c08cb87a55fe31baa4c8b3c32db9af4c12
Viraj76 <virajg.7680@gmail.com>
2023-06-04 16:59:56 +0530

    Item view for showing client who rated contractor

---
+++ app/src/main/java/com/example/bottomnavigation/contractor/adapter/RatingHistoryAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.bottomnavigation.contractor.adapter
+
+class RatingHistoryAdapter {
+}
---
+++ app/src/main/java/com/example/bottomnavigation/models/ClientWhoRated.kt
@@ -0,0 +1,3 @@
+package com.example.bottomnavigation.models
+
+data class ClientWhoRated()
