ead7dade034865dd6082ec7d7bb0cc36dab9f21a
Viraj76 <virajg.7680@gmail.com>
2023-06-04 17:01:25 +0530

    Models

--- app/src/main/java/com/example/bottomnavigation/models/ClientWhoRated.kt
+++ app/src/main/java/com/example/bottomnavigation/models/ClientWhoRated.kt
@@ -1,3 +1,7 @@
 package com.example.bottomnavigation.models
 
-data class ClientWhoRated()
+data class ClientWhoRated(
+    val clientName : String? = null,
+    val date  : String? = null,
+    val rating  : String? = null,
+)
--- app/src/main/java/com/example/bottomnavigation/models/RatedContractor.kt
+++ app/src/main/java/com/example/bottomnavigation/models/RatedContractor.kt
@@ -2,5 +2,6 @@ package com.example.bottomnavigation.models
 
 data class RatedContractor (
     val clientId : String ? = null,
-    val rating : String ? = null
+    val rating : String ? = null,
+    val rateDate : String ? = null
         )
