3a6b46756963ee46258aaffc3b0fd6c77d5c0dd6
Viraj76 <virajg.7680@gmail.com>
2023-06-11 14:33:16 +0530

    Shifted content from notification to chat fragment to do lauda lassan

--- app/src/main/java/com/example/bottomnavigation/client/fragments/ChatFragment.kt
+++ app/src/main/java/com/example/bottomnavigation/client/fragments/ChatFragment.kt
@@ -1,59 +1,61 @@
-package com.example.bottomnavigation
+package com.example.bottomnavigation.client.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.bottomnavigation.R
+import com.example.bottomnavigation.chat.NotificationFragmentAdapter
+import com.example.bottomnavigation.databinding.FragmentChatBinding
+import com.example.bottomnavigation.databinding.FragmentNotificationBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ChatFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ChatFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding : FragmentChatBinding
+    private lateinit var notificationFragmentAdapter: NotificationFragmentAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+    ): View {
+        binding = FragmentChatBinding.inflate(layoutInflater)
+        prepareChatFragmentAdapterForShowingContractors()
+        showingContractors()
+        return binding.root
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChatFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChatFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun prepareChatFragmentAdapterForShowingContractors() {
+        notificationFragmentAdapter = NotificationFragmentAdapter(requireContext())
+        binding.rvChats.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
+            adapter = notificationFragmentAdapter
+        }
+    }
+    private fun showingContractors() {
+        val currentUserId = FirebaseAuth.getInstance().currentUser?.uid
+        FirebaseDatabase.getInstance().getReference("Chatbase")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val clientsIdList = ArrayList<String>()
+                    val chatRoomList = ArrayList<String>()
+                    for(chatIds in snapshot.children){
+                        if(chatIds.key!!.contains(currentUserId!!)){
+                            chatRoomList.add(chatIds.key!!)
+                            clientsIdList.add(chatIds.key!!.replace(currentUserId,""))
+                        }
+                    }
+                    notificationFragmentAdapter.setClientIdList(clientsIdList)
+                    notificationFragmentAdapter.setChatRoom(chatRoomList)
                 }
-            }
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
     }
 }
--- app/src/main/java/com/example/bottomnavigation/client/fragments/NotificationFragment.kt
+++ app/src/main/java/com/example/bottomnavigation/client/fragments/NotificationFragment.kt
@@ -19,59 +19,15 @@ import com.google.firebase.database.ValueEventListener
 class NotificationFragment : Fragment() {
 
     private lateinit var binding : FragmentNotificationBinding
-    private lateinit var notificationFragmentAdapter: NotificationFragmentAdapter
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View{
         binding = FragmentNotificationBinding.inflate(layoutInflater)
-        binding.notificationToolbar.apply {
-            title = "Notification"
-            (activity as AppCompatActivity).setSupportActionBar(this)
-        }
-        prepareChatFragmentAdapterForShowingContractors()
-
-        showingContractors()
-
-
         return binding.root
     }
 
-    private fun showingContractors() {
-        val currentUserId = FirebaseAuth.getInstance().currentUser?.uid
-        FirebaseDatabase.getInstance().getReference("Chatbase")
-            .addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    val clientsIdList = ArrayList<String>()
-                    val chatRoomList = ArrayList<String>()
 
-                    for(chatIds in snapshot.children){
-                        if(chatIds.key!!.contains(currentUserId!!)){
-                            chatRoomList.add(chatIds.key!!)
-                            clientsIdList.add(chatIds.key!!.replace(currentUserId,""))
-                        }
-                    }
-
-                    notificationFragmentAdapter.setClientIdList(clientsIdList)
-                    notificationFragmentAdapter.setChatRoom(chatRoomList)
-
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    TODO("Not yet implemented")
-                }
-
-            })
-    }
-
-    private fun prepareChatFragmentAdapterForShowingContractors() {
-        notificationFragmentAdapter = NotificationFragmentAdapter(requireContext())
-        binding.contractorsRv.apply {
-            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
-            adapter = notificationFragmentAdapter
-        }
-    }
 
 
 }
