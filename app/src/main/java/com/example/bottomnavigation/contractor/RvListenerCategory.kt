d8f48d40cdb8c25749f61e510f3cbceee6f525d4
Viraj76 <virajg.7680@gmail.com>
2023-07-22 23:00:09 +0530

    interface listener for category touch

--- app/src/main/java/com/example/bottomnavigation/contractor/RvListenerCategory.kt
+++ app/src/main/java/com/example/bottomnavigation/contractor/RvListenerCategory.kt
@@ -1,4 +1,7 @@
 package com.example.bottomnavigation.contractor
 
+import com.example.bottomnavigation.models.Category
+
 interface RvListenerCategory {
+    fun onCategoryClick(category: Category)
 }
