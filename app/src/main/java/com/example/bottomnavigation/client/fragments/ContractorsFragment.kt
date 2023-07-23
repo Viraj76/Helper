591cdc9c5b0b769e3a5793e9bf5c01b7ed4e56c3
Viraj76 <virajg.7680@gmail.com>
2023-07-23 20:47:26 +0530

    designed the contractor fragment

--- app/src/main/java/com/example/bottomnavigation/client/fragments/ContractorsFragment.kt
+++ app/src/main/java/com/example/bottomnavigation/client/fragments/ContractorsFragment.kt
@@ -1,60 +1,90 @@
 package com.example.bottomnavigation.client.fragments
 
+import ContractorsViewModel
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bottomnavigation.R
+import com.example.bottomnavigation.client.adapter.ContractorsAdapter
+import com.example.bottomnavigation.databinding.FragmentContractorsBinding
+import com.example.bottomnavigation.models.ContractorID
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ContractorsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ContractorsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentContractorsBinding
+    private lateinit var contractorsAdapter: ContractorsAdapter
+    private val viewModel: ContractorsViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contractors, container, false)
+    ): View {
+        binding = FragmentContractorsBinding.inflate(layoutInflater)
+
+        prepareRvForContractorAdapter()
+        setupSearch()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ContractorsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ContractorsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun prepareRvForContractorAdapter() {
+        contractorsAdapter = ContractorsAdapter()
+        binding.rvContractors.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            adapter = contractorsAdapter
+        }
+    }
+
+    private fun setupSearch() {
+        binding.searchEt.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val query = s.toString().trim()
+                contractorsAdapter.filter.filter(query)
             }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (viewModel.contractorList.isEmpty()) {
+            // Fetch data from Firebase only if the contractorList is empty
+            showingAllContractors()
+        } else {
+            // If the contractorList is not empty, set the list to the adapter
+            contractorsAdapter.setContractorList(viewModel.contractorList)
+        }
+    }
+
+    private fun showingAllContractors() {
+        FirebaseDatabase.getInstance().getReference("Contractor Id's")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    viewModel.contractorList.clear() // Clear the list before adding new data
+                    for (allDetails in snapshot.children) {
+                        val detail = allDetails.getValue(ContractorID::class.java)
+                        detail?.let { viewModel.contractorList.add(it) }
+                    }
+                    contractorsAdapter.setContractorList(viewModel.contractorList)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Handle Firebase error, if needed
+                }
+            })
     }
 }
