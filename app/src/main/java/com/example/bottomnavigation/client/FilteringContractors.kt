90690f9b1dd7ed11d0c6b5e4fda77f4a7ff4562c
Viraj76 <virajg.7680@gmail.com>
2023-07-23 20:50:29 +0530

    class for filtering the contractors

--- app/src/main/java/com/example/bottomnavigation/client/FilteringContractors.kt
+++ app/src/main/java/com/example/bottomnavigation/client/FilteringContractors.kt
@@ -1,4 +1,42 @@
 package com.example.bottomnavigation.client
 
-class FilteringContractors {
+import android.widget.Filter
+import com.example.bottomnavigation.client.adapter.ContractorsAdapter
+import com.example.bottomnavigation.models.ContractorID
+import java.util.*
+import kotlin.collections.ArrayList
+
+class FilteringContractors(
+    private val adapter : ContractorsAdapter,
+    private val filterContractors : ArrayList<ContractorID>
+) : Filter(){
+    override fun performFiltering(constraint: CharSequence?): FilterResults {
+        val results = FilterResults()
+        if(!constraint.isNullOrEmpty()){
+            val query = constraint.toString().trim().uppercase(Locale.getDefault())
+            val keyword = query.split(" ")
+            val filteredContractors = kotlin.collections.ArrayList<ContractorID>()
+
+            for(contractor in filterContractors){
+                if(keyword.any{ keyword->
+                    contractor.address?.uppercase(Locale.getDefault())?.contains(keyword)==true||
+                    contractor.businessType?.uppercase(Locale.getDefault())?.contains(keyword)==true
+                    }){
+                    filteredContractors.add(contractor)
+                }
+            }
+            results.count = filteredContractors.size
+            results.values = filteredContractors
+        }else{
+            results.count = filterContractors.size
+            results.values = filterContractors
+        }
+        return results
+    }
+
+    override fun publishResults(constraint: CharSequence?, results: FilterResults) {
+        adapter.contractorsList = results.values as ArrayList<ContractorID>
+        adapter.notifyDataSetChanged()
+    }
+
 }
